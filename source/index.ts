51fe04cd2c634bdfa32a8b24b2885ba77bea073b
Aman <amanb218@gmail.com>
2021-10-24 13:44:24 +0530

    refactor: updated requested changes

--- source/index.ts
+++ source/index.ts
@@ -1,73 +1,73 @@
-import ky from "ky-universal";
-import isIp from "is-ip";
+import ky from "ky-universal"
+import isIp from "is-ip"
 
 interface IpApiData {
-	ip: string;
-	city: string;
-	reserved: boolean;
-	region: string;
-	region_code: string;
-	country: string;
-	country_code: string;
-	country_code_iso3: string;
-	country_capital: string;
-	country_tld: string;
-	country_name: string;
-	continent_code: string;
-	in_eu: boolean;
-	postal: string;
-	latitude: number;
-	longitude: number;
-	timezone: string;
-	utc_offset: string;
-	country_calling_code: string;
-	currency: string;
-	currency_name: string;
-	languages: string;
-	country_area: number;
-	country_population: number;
+	ip: string
+	city: string
+	reserved: boolean
+	region: string
+	region_code: string
+	country: string
+	country_code: string
+	country_code_iso3: string
+	country_capital: string
+	country_tld: string
+	country_name: string
+	continent_code: string
+	in_eu: boolean
+	postal: string
+	latitude: number
+	longitude: number
+	timezone: string
+	utc_offset: string
+	country_calling_code: string
+	currency: string
+	currency_name: string
+	languages: string
+	country_area: number
+	country_population: number
 }
 
 declare namespace ipLocation {
 	export interface LocationData {
-		latitude: number;
-		longitude: number;
-		city: string;
+		latitude: number
+		longitude: number
+		city: string
 		region: {
-			name: string;
-			code: string;
-		};
+			name: string
+			code: string
+		}
 		country: {
-			name: string;
-			code: string;
-			iso3: string;
-			capital: string;
-			tld: string;
-			population: number;
-			area: number;
-			callingCode: string;
-			postalCode: string;
+			name: string
+			code: string
+			iso3: string
+			capital: string
+			tld: string
+			population: number
+			area: number
+			callingCode: string
+			postalCode: string
 			timezone: {
-				code: string;
-				offset: string;
-			};
+				code: string
+				offset: string
+			}
 			currency: {
-				name: string;
-				code: string;
+				name: string
+				code: string
 			};
-			languages: string[];
-		};
+			languages: string[]
+		}
 		continent: {
-			code: string;
-			inEu: boolean;
-		};
+			code: string
+			inEu: boolean
+		}
 	}
 
 	export interface ReservedData {
-		reserved: boolean;
+		reserved: boolean
 	}
 
-	export type ReturnType = (LocationData & ReservedData) | ReservedData;
+	export type ReturnType = (LocationData & ReservedData) | ReservedData
 }
 
 /**
@@ -83,82 +83,58 @@ const ipLocation = require("ip-location");
 })();
 ```
 */
-async function ipLocation(
-	ip: string,
-	api_key: string = ""
-): Promise<ipLocation.ReturnType> {
+async function ipLocation(ip: string, apiKey?: string): Promise<ipLocation.ReturnType> {
 	if (typeof ip !== "string" || !isIp.v4(ip)) {
-		throw new TypeError("A valid ipv4 address must be provided!");
+		throw new TypeError("A valid ipv4 address must be provided!")
 	}
 
-	const {
+	const IPAPI_URL = new URL(`https://ipapi.co/${ip}/json/`)
+
+	if (apiKey) {
+		IPAPI_URL.searchParams.append("key", apiKey)
+	}
+
+	const { latitude, longitude, city, reserved, region, region_code, country_name, country_code, country_code_iso3, country_capital, country_tld, country_population, country_calling_code, continent_code, in_eu, postal, timezone, utc_offset, currency, currency_name, languages, country_area }: IpApiData = await ky(IPAPI_URL.href).json()
+
+	return reserved ? {
+		reserved,
+	} : {
 		latitude,
 		longitude,
 		city,
-		reserved,
-		region,
-		region_code,
-		country_name,
-		country_code,
-		country_code_iso3,
-		country_capital,
-		country_tld,
-		country_population,
-		country_calling_code,
-		continent_code,
-		in_eu,
-		postal,
-		timezone,
-		utc_offset,
-		currency,
-		currency_name,
-		languages,
-		country_area,
-	}: IpApiData = await ky(
-		`https://ipapi.co/${ip}/json/${api_key ? `?key=${api_key}` : ""}`
-	).json();
-
-	return reserved
-		? {
-				reserved,
-		  }
-		: {
-				latitude,
-				longitude,
-				city,
-				reserved: Boolean(reserved),
-				region: {
-					name: region,
-					code: region_code,
-				},
-				country: {
-					name: country_name,
-					code: country_code,
-					iso3: country_code_iso3,
-					capital: country_capital,
-					tld: country_tld,
-					population: country_population,
-					area: country_area,
-					callingCode: country_calling_code,
-					postalCode: postal,
-					timezone: {
-						code: timezone,
-						offset: utc_offset,
-					},
-					currency: {
-						name: currency_name,
-						code: currency,
-					},
-					languages: languages ? languages.split(",") : [],
-				},
-				continent: {
-					code: continent_code,
-					inEu: in_eu,
-				},
-		  };
+		reserved: Boolean(reserved),
+		region: {
+			name: region,
+			code: region_code,
+		},
+		country: {
+			name: country_name,
+			code: country_code,
+			iso3: country_code_iso3,
+			capital: country_capital,
+			tld: country_tld,
+			population: country_population,
+			area: country_area,
+			callingCode: country_calling_code,
+			postalCode: postal,
+			timezone: {
+				code: timezone,
+				offset: utc_offset
+			},
+			currency: {
+				name: currency_name,
+				code: currency
+			},
+			languages: languages ? languages.split(",") : [],
+		},
+		continent: {
+			code: continent_code,
+			inEu: in_eu
+		}
+	}
 }
 
-export default ipLocation;
+export default ipLocation
 
-module.exports = ipLocation;
-module.exports.default = ipLocation; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
+module.exports = ipLocation
+module.exports.default = ipLocation // eslint-disable-line @typescript-eslint/no-unsafe-member-access
